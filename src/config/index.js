d6c9abefaa894696fad0b6afa37c3814d32e95a9
Roney C. Lima <69720380+RoneyLima@users.noreply.github.com>
2021-03-17 13:30:30 -0300

    config

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const config = {}
+
+config.JWT_KEY = 'bvz-identifyByJsonWebTokenOnAPIserver';
+config.URL_MONGO = 'mongodb://localhost/bvz-testes';
+config.SALT_ROUNDS = 10;
+
+module.exports = config;
