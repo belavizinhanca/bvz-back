8c027721611d07b4b66a6811d4bf14edd1391298
Roney C. Lima <69720380+RoneyLima@users.noreply.github.com>
2021-03-17 13:35:59 -0300

    subindo a config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {}
+
+
+config.URL_MONGO = "mongodb+srv://BVZ:iCHkmDlW31WMycKr@bvz1.rwmza.mongodb.net/bdbvz?retryWrites=true&w=majority";
+config.JWT_KEY = "bvz-jwt-key-api-login"
+
+module.exports = config;
