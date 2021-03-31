b7b72d682e4a14857a08017163fd75eb6fe2ebba
Roney C. Lima <69720380+RoneyLima@users.noreply.github.com>
2021-03-30 22:52:29 -0300

    Correção do path db

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 const config = {}
 
 config.JWT_KEY = 'bvz-identifyByJsonWebTokenOnAPIserver';
-config.URL_MONGO = 'mongodb://localhost/bvz-testes';
+
+// config.URL_MONGO = 'mongodb://localhost/bvz-testes'; //db local para testes
+config.URL_MONGO = 'mongodb+srv://BVZ:iCHkmDlW31WMycKr@bvz1.rwmza.mongodb.net/bdbvz?retryWrites=true&w=majority'; //db atlas versão oficial
 config.SALT_ROUNDS = 10;
 
 module.exports = config;
